935c92d05604694c2562e5ca04483db8b2af1860
VIVEK V NAIR <viveknair6915@gmail.com>
2024-01-16 02:28:12 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,117 @@
+export const exploreWorlds = [
+  {
+    id: 'Murder-1',
+    imgUrl: '/murder1.png',
+    title: 'Way to hacking',
+  },
+  {
+    id: 'Murder-2',
+    imgUrl: '/murder2.png',
+    title: 'The Night stalker',
+  },
+  {
+    id: 'murder-3',
+    imgUrl: '/murder3.png',
+    title: 'Myterious Witch',
+  },
+  {
+    id: 'murder-4',
+    imgUrl: '/murder4.png',
+    title: 'The Death End',
+  },
+  {
+    id: 'Murder-5',
+    imgUrl: '/murder5.png',
+    title: 'The unknown',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'WINNER',
+    subtitle:
+        'Prize will be given to a project that outstands all other submissions.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: '1st RUNNER UP',
+    subtitle:
+        '1st Runner-up prize will be given to the second best project of the hackathon.',
+  },
+];
+export const newFeatures2 = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: '2nd RUNNER UP',
+    subtitle:
+        '2nd Runner-up project of the hackathon will win some awesome prizes.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'ALL GIRLS TEAM',
+    subtitle:
+        'Will be awarded to the project of hackathon which was made by an all girls team.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/image32.jpeg',
+    title: ' Who can attend?',
+    subtitle: 'Hackstreet is open to students of all colleges.',
+  },
+  {
+    imgUrl: '/image30.jpeg',
+    title: ' I am a first time hacker, what should I do?',
+    subtitle:
+      '  No worries, we love to welcome first time hackers and start their hacking journey from a hackathon. We have some awesome workshops and events which will help you get started with hackathon and hacking in general.',
+  },
+  {
+    imgUrl: '/image36.jpeg',
+    title: ' How team formation works?',
+    subtitle:
+      "You can form team with 4 members in it at maximum. If you don't have a team, there'll be plenty of opportunities for you to meet hackers and form a team!",
+  },
+  {
+    imgUrl: '/image34.jpeg',
+    title: '   How to register myself in the hackathon??',
+    subtitle:
+      'All you need is to fill our form above and join Discord, we will guide you through everything there',
+  },
+  {
+    imgUrl: '/image37.jpeg',
+    title: '   I have more questions?',
+    subtitle:
+      '    Reach us directly on our Discord Server we would be happy to help you.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'telegram',
+    url: '/telegram.webp',
+    href: 'https://t.me/joinchat/JkKFGhgrkuLYiwiSToT_Jg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+    href: 'https://www.linkedin.com/authwall?trk=bf&trkInfo=AQFx5DDyMtDHlwAAAYzdvY-IdZOXMNqamWkeKzZQdyYytS5v4rnk6NUb7efZRQdNWS1-zOQS7SQ0ifW0sKyRTagpY_ksTcJ9QwPZDXBAz-WuBhgRWZ-UZbdl_DXBh6kcbYZjf40=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fieee-student-branch-jiit',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+    href: 'https://www.instagram.com/ieeesbjiit/',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+    href: 'https://www.facebook.com/ieeesbjiit/',
+  },
+];
