2fc3645f6691eb8a2e165eeef424135f6aef1cae
Khushi Agarwal <kagarwal1676@gmail.com>
2024-01-16 23:54:38 +0530

    resolve error

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,24 @@
-"use client";
+'use client';
 
-import Link from "next/link";
+import Link from 'next/link';
 
-import "./Navbar.css";
+import './Navbar.css';
 
-export default function Navbar() {
+function Navbar() {
   return (
-    <div className="nav">
-      <div className="heading">
+    <div className='nav'>
+      <div className='heading'>
         <h2>HACKSTREET 2.0</h2>
       </div>
-      <div className="NavBar-desc">
-        <Link href="/">HOME</Link>
-        <Link href="#about">ABOUT</Link>
-        <Link href="#prize">PRIZE</Link>
-        <Link href="#faq">FAQs</Link>
-        <Link href="#sponser">SPONSERS</Link>
-        <button type="button" className="devfolio">
+      <div className='NavBar-desc'>
+        <Link href='/'>HOME</Link>
+        <Link href='#about'>ABOUT</Link>
+        <Link href='#prize'>PRIZE</Link>
+        <Link href='#faq'>FAQs</Link>
+        <Link href='#sponser'>SPONSERS</Link>
+        <button type='button' className='devfolio'>
           <span>
-            <a href="https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W">
+            <a href='https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W'>
               DEVFOLIO
             </a>
           </span>
@@ -27,3 +27,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+export default Navbar;
