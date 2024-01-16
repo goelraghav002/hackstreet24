6c19a076d52de16d12380d2a88eacdf7e524fab6
Khushi Agarwal <kagarwal1676@gmail.com>
2024-01-16 23:48:22 +0530

    error in navbar resolving

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,34 +1,31 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div className="nav">
-    <div className="heading">
-      <h2>HACKSTREET 2.0</h2>
+      <div className="heading">
+        <h2>HACKSTREET 2.0</h2>
+      </div>
+      <div className="NavBar-desc">
+        <Link href="/">HOME</Link>
+        <Link href="#about">ABOUT</Link>
+        <Link href="#prize">PRIZE</Link>
+        <Link href="#faq">FAQs</Link>
+        <Link href="#sponser">SPONSERS</Link>
+        <button type="button" className="devfolio">
+          <span>
+            <a href="https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W">
+              DEVFOLIO
+            </a>
+          </span>
+        </button>
+      </div>
     </div>
-    <div className="NavBar-desc">
-      <Link href="/">HOME</Link>
-      <Link href="#about">ABOUT</Link>
-      <Link href="#prize">PRIZE</Link>
-      <Link href="#faq">FAQs</Link>
-      <Link href="#sponser">SPONSERS</Link>   
-      <button
-        type="button"
-        className="devfolio"
-      >
-        <span>
-          <a href="https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W">
-            DEVFOLIO
-          </a>
-        </span>
-      </button>
-    </div>
-  </div>
-  )
+  );
 }
 
 export default Navbar;
