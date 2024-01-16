7476b53f3978ba7338ba105dd98b9d5ef3fb1f95
Khushi Agarwal <kagarwal1676@gmail.com>
2024-01-16 23:59:45 +0530

    resolve

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,19 +6,19 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='nav'>
-      <div className='heading'>
+    <div className="nav">
+      <div className="heading">
         <h2>HACKSTREET 2.0</h2>
       </div>
-      <div className='NavBar-desc'>
-        <Link href='/'>HOME</Link>
-        <Link href='#about'>ABOUT</Link>
-        <Link href='#prize'>PRIZE</Link>
-        <Link href='#faq'>FAQs</Link>
-        <Link href='#sponser'>SPONSERS</Link>
-        <button type='button' className='devfolio'>
+      <div className="NavBar-desc">
+        <Link href="/">HOME</Link>
+        <Link href="#about">ABOUT</Link>
+        <Link href="#prize">PRIZE</Link>
+        <Link href="#faq">FAQs</Link>
+        <Link href="#sponser">SPONSERS</Link>
+        <button type="button" className="devfolio">
           <span>
-            <a href='https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W'>
+            <a href="https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W">
               DEVFOLIO
             </a>
           </span>
