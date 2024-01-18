b6a03246b1165b73edff761dec08a053ed130001
Abhinav Mishra <mishraabhinav2306@gmail.com>
2024-01-18 05:59:43 +0530

    responsive navbar with menu button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,9 +2,15 @@
 
 import Link from 'next/link';
 
+import React from 'react';
 import './Navbar.css';
 
 function Navbar() {
+  const [status, setStatus] = React.useState(false);
+  const handleClick = () => {
+    setStatus(!status);
+  }
+  
   return (
     <div className="nav">
       <div className="heading">
@@ -16,7 +22,7 @@ function Navbar() {
         <Link href="#prize">PRIZE</Link>
         <Link href="#faq">FAQs</Link>
         <Link href="#sponser">SPONSERS</Link>
-        <button type="button" className="devfolio">
+        <button type="button"  className="devfolio">
           <span>
             <a href="https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W">
               DEVFOLIO
@@ -24,16 +30,18 @@ function Navbar() {
           </span>
         </button>
       </div>
-      <button type="button" className="menuButton">
-        <img id="menuImg" src="/menu.png" />
+      <button type="button" onClick={handleClick} className="menuButton">
+        <img id="menuImg" src="/menuBtn.png" />
       </button>
 
-      <div className="NavBar-description">
-        <Link href="/">HOME</Link>
-        <Link href="#about">ABOUT</Link>
-        <Link href="#prize">PRIZE</Link>
-        <Link href="#faq">FAQs</Link>
-        <Link href="#sponser">SPONSERS</Link>
+      <div style={{ display: status ? 'block' : 'none' }} className="NavBar-description flex flex-col">
+        <button type="button" onClick={handleClick}className='crossImg'><img src="/whiteCross.png"/></button>
+
+        <Link className="navParts" href="/">HOME</Link><br/>
+        <Link className="navParts" href="#about">ABOUT</Link><br/>
+        <Link className="navParts" href="#prize">PRIZE</Link><br/>
+        <Link className="navParts" href="#faq">FAQs</Link><br/>
+        <Link className="navParts" href="#sponser">SPONSERS</Link><br/>
         <button type="button" className="devfolio-btn">
           <span>
             <a href="https://youtu.be/dQw4w9WgXcQ?si=LEaggX_fLM_s3I1W">
