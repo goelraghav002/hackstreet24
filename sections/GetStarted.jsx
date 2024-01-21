d6abb2867b1c08747ccce0046f773dbfbbb29707
Raghav Goel <goelraghav002@gmail.com>
2024-01-21 14:32:55 +0530

    fixed timeline texr

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -16,92 +16,92 @@ function GetStarted() {
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} flex lg:flex-col flex-col gap-8`}
+        className={`${styles.innerWidth} flex lg:flex-col flex-col gap-8 m-auto`}
       >
-        <h1 className="heading2"> TIMELINE </h1>
+        <TitleText title="Timeline" textStyles="text-center" />
       </motion.div>
       <motion.div
         initial="hidden"
         whileInView="show"
         variants={fadeIn("down", "spring", 0.2, 1.5)}
       >
-      <div className="timeline">
-        <motion.div
-          initial="hidden"
-          whileInView="show"
-          variants={fadeIn("right", "spring", 0.8, 1.5)}
-        >
-          <div className="container left">
-            <img src="/stopwatch.jpeg" />
-            <div className="text-box">
-              <h2 className="heading1">REGISTRATIONS</h2>
-              <small>29TH JANUARY, 2023</small>
-              <p>11:00 AM onwards</p>
-              <span className="left-arrow"></span>
+        <div className="timeline">
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={fadeIn("right", "spring", 0.8, 1.5)}
+          >
+            <div className="container left">
+              <img src="/stopwatch.jpeg" />
+              <div className="text-box">
+                <h2 className="heading1">REGISTRATIONS</h2>
+                <small>29TH JANUARY, 2023</small>
+                <p>11:00 AM onwards</p>
+                <span className="left-arrow"></span>
+              </div>
             </div>
-          </div>
-        </motion.div>
-        <motion.div
-          initial="hidden"
-          whileInView="show"
-          variants={fadeIn("left", "spring", 0.9, 1.5)}
-        >
-          <div className="container right">
-            <img src="/two.jpg" />
-            <div className="text-box">
-              <h2 className="heading1">HACKING PERIOD STARTS</h2>
-              <small>3RD FEBRUARY, 2023</small>
-              <p>11:00 AM onwards</p>
-              <span className="right-arrow"></span>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={fadeIn("left", "spring", 0.9, 1.5)}
+          >
+            <div className="container right">
+              <img src="/two.jpg" />
+              <div className="text-box">
+                <h2 className="heading1">HACKING PERIOD STARTS</h2>
+                <small>3RD FEBRUARY, 2023</small>
+                <p>11:00 AM onwards</p>
+                <span className="right-arrow"></span>
+              </div>
             </div>
-          </div>
-        </motion.div>
-        <motion.div
-          initial="hidden"
-          whileInView="show"
-          variants={fadeIn("right", "spring", 1, 1.5)}
-        >
-          <div className="container left">
-            <img src="/stopwatch.jpeg" />
-            <div className="text-box">
-              <h2 className="heading1">MID EVALUATION</h2>
-              <small>3RD FEBRUARY, 2023</small>
-              <p>11:00 AM onwards</p>
-              <span className="left-arrow"></span>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={fadeIn("right", "spring", 1, 1.5)}
+          >
+            <div className="container left">
+              <img src="/stopwatch.jpeg" />
+              <div className="text-box">
+                <h2 className="heading1">MID EVALUATION</h2>
+                <small>3RD FEBRUARY, 2023</small>
+                <p>11:00 AM onwards</p>
+                <span className="left-arrow"></span>
+              </div>
             </div>
-          </div>
-        </motion.div>
-        <motion.div
-          initial="hidden"
-          whileInView="show"
-          variants={fadeIn("left", "spring", 1.1, 1.5)}
-        >
-          <div className="container right">
-            <img src="/two.jpg" />
-            <div className="text-box">
-              <h2 className="heading1">HACKING PERIOD ENDS</h2>
-              <small>4TH FEBRUARY, 2023</small>
-              <p>11:00 AM onwards</p>
-              <span className="right-arrow"></span>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={fadeIn("left", "spring", 1.1, 1.5)}
+          >
+            <div className="container right">
+              <img src="/two.jpg" />
+              <div className="text-box">
+                <h2 className="heading1">HACKING PERIOD ENDS</h2>
+                <small>4TH FEBRUARY, 2023</small>
+                <p>11:00 AM onwards</p>
+                <span className="right-arrow"></span>
+              </div>
             </div>
-          </div>
-        </motion.div>
-        <motion.div
-          initial="hidden"
-          whileInView="show"
-          variants={fadeIn("right", "spring", 1.2, 1.5)}
-        >
-          <div className="container left">
-            <img src="/stopwatch.jpeg" />
-            <div className="text-box">
-              <h2 className="heading1">FINAL EVALUATION</h2>
-              <small>4TH FEBRUARY, 2023</small>
-              <p>2PM onwards</p>
-              <span className="left-arrow"></span>
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={fadeIn("right", "spring", 1.2, 1.5)}
+          >
+            <div className="container left">
+              <img src="/stopwatch.jpeg" />
+              <div className="text-box">
+                <h2 className="heading1">FINAL EVALUATION</h2>
+                <small>4TH FEBRUARY, 2023</small>
+                <p>2PM onwards</p>
+                <span className="left-arrow"></span>
+              </div>
             </div>
-          </div>
-        </motion.div>
-      </div>
+          </motion.div>
+        </div>
       </motion.div>
     </section>
   );
