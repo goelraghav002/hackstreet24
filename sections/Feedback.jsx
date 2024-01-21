554885f4d0da49f7de95c63b977379effbf9b63e
Abhinav Mishra <mishraabhinav2306@gmail.com>
2024-01-21 18:07:40 +0530

    sponsors heading choti kri

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -15,7 +15,9 @@ function Feedback() {
       
       <div>
         <div className="sponsors-section">
-        <TitleText title="Gold Partners" textStyles="text-center"/>
+          <h4 className="font-normal justify-center flex  lg:text-[42px] text-[26px] text-white">
+            Gold Partners
+          </h4>
       <div className="sponsors" id="gsp">
         <img className="sponsorsImages" id="dev" src="/devfolio_spon.webp" alt="" />
         <img className="sponsorsImages" id="polygon" src="/polygon_spon.png" alt="" />
@@ -23,34 +25,44 @@ function Feedback() {
       </div>
       
         <div>
-        <TitleText title="Silver Partner" textStyles="text-center" />
+          <h4 className="font-normal justify-center flex  lg:text-[42px] text-[26px] text-white">
+            Silver Partner
+          </h4>
       <div className="sponsors">
         <img className="sponsorsImages" id="replit" src="/ReplitImg-.png" alt="" />
       </div>
         </div>
 
         <div>
-        <TitleText title="API Partner" textStyles="text-center" />
+          <h4 className="font-normal justify-center flex  lg:text-[42px] text-[26px] text-white">
+            API Partner
+          </h4>
       <div className="sponsors">
         <img className="sponsorsImages" id="bee" src="/BeeceptorImg.png" alt="" />
       </div>
         </div>
         
         <div>
-          <TitleText title="Interview Partners" textStyles="text-center" />
+          <h4 className="font-normal justify-center flex  lg:text-[42px] text-[26px] text-white">
+            Interview Partners
+          </h4>
       <div className="sponsors">
         <img className="sponsorsImages" id="buddy" src="\InterviewBuddyImg.png" alt="" />
         <img className="sponsorsImages" id="cake" src="\interviewcakeImg.png" alt="" />
       </div></div>
         
         <div>
-          <TitleText title="Certificate Partner" textStyles="text-center" />
+          <h4 className="font-normal justify-center flex  lg:text-[42px] text-[26px] text-white">
+            Certificate Partner
+          </h4>
       <div className="sponsors">
         <img className="sponsorsImages" id="certi" src="\givemycertificate_spon.png" alt="" />
       </div></div>
         
         <div>
-        <TitleText title="In-Kind Partners" textStyles="text-center" />
+          <h4 className="font-normal justify-center flex  lg:text-[42px] text-[26px] text-white">
+            In-Kind Partners
+          </h4>
       <div className="sponsors inkind-sp">
         <img className="sponsorsImages" id="alpha" src="\WolframAlphaImg.png" alt="" />
         <img className="sponsorsImages" id="echo3d" src="\echo3DImg.png" alt="" />
