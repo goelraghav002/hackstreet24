71b6fd592e75e9487881a1cba464fe2f16980a94
Siddhi Singhal <singhalsiddhi5@gmail.com>
2024-02-02 16:41:45 +0530

    timeline-one item added

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -91,13 +91,29 @@ function GetStarted() {
             <div className="container left">
               <img src="/stopwatch.jpeg" />
               <div className="text-box">
-                <h2 className="heading1">FINAL EVALUATION</h2>
+                <h2 className="heading1">PROJECT SUBMISSION</h2>
                 <small>4TH FEBRUARY, 2024</small>
                 <p>3:00 PM onwards</p>
                 <span className="left-arrow" />
               </div>
             </div>
           </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={fadeIn('left', 'spring', 1.2, 1.5)}
+          >
+            <div className="container right">
+              <img src="/two.jpg" />
+              <div className="text-box">
+                <h2 className="heading1">FINAL EVALUATION</h2>
+                <small>4TH FEBRUARY, 2024</small>
+                <p>3:30 PM onwards</p>
+                <span className="right-arrow" />
+              </div>
+            </div>
+          </motion.div>
+          
         </div>
       </motion.div>
     </section>
